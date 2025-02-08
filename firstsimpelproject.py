84c2aa1964c225b4255d160156a599f8b25c022e
fatemehroohbakhsh <127182923+fatemehroohbakhsh@users.noreply.github.com>
2025-02-08 11:13:04 +0330

    Update firstsimpelproject.py

--- firstsimpelproject.py
+++ firstsimpelproject.py
@@ -1,18 +1,22 @@
-class car:
+class Car:
     def __init__(self,speed,year,model,brand):
-        self.s=speed
-        self.y=year
-        self.m=model
-        self.b=brand
+        self.speed=speed
+        self.year=year
+        self.model=model
+        self.brand=brand
     def accelerate(self,amount):
-        (self.s)+=amount
-        print(self.s)
+        self.speed+=amount
+        print(self.speed)
     def brake(self,amount):
-        (self.s)-=amount
-        print(self.s)
+        self.speed-=amount
+        if self.speed<0:
+            print("it is not possible")   
+            self.speed=0       #if speed is negtive speed=0
+        else:
+          print(self.speed)     
     def display_info(self):
-        print(f"speed:{self.s} and year:{self.y} and model:{self.m} and brand:{self.b}")  
-x4=car(0,2000,"a","bmw")
+        print(f"speed:{self.speed} and year:{self.year} and model:{self.model} and brand:{self.brand}")  
+x4=Car(0,2000,"a","bmw")     #making an object
 x4.accelerate(80)
 x4.brake(40)
 x4.display_info()     
